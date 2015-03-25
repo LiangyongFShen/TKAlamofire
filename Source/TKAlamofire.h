bd64ec0f9a4e24ece1854c28d83aa3c78202d520
Nicolastinkl <nicolastinkl@gmail.com>
2015-03-25 17:43:22 +0800

    update

---
+++ Source/TKAlamofire.h
@@ -0,0 +1,19 @@
+//
+//  TKAlamofire.h
+//  TKAlamofire
+//
+//  Created by tinkl on 25/3/15.
+//  Copyright (c) 2015 ___TK___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TKAlamofire.
+FOUNDATION_EXPORT double TKAlamofireVersionNumber;
+
+//! Project version string for TKAlamofire.
+FOUNDATION_EXPORT const unsigned char TKAlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKAlamofire/PublicHeader.h>
+
+
